265d670b57dd047ab3691b72c5b5364492be1cc2
Gaurav <gauravgjee2025@gmail.com>
2026-05-27 09:08:25 +0530

    signup

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,29 +1,28 @@
 import { json, Router } from "express";
 import { passwordValidate } from "../utils/passwordValidate";
-import { signupUser } from "../service/supabaseService";
+import { signupUser } from "../services/supabaseService";
 const router = Router();
 
-router.post('/signup',(req,res)=>{
+router.post('/signup', async (req,res)=>{
 
-    if(passwordValidate(req.body.pass)){
-        try{
+    try{
+        if(passwordValidate(req.body.pass)){
             const data = await signupUser(
-                req.body.email,
+                req.body.email ,
                 req.body.pass
             );
-
-            return res.json(data);
+        return res.json(data);
         }
-        catch(error){
+        else {
             return res.json({
-                message : error.message
+                message : "Invalid password"
             });
         }
-
     }
-    else {
-        return res.json({message : "Inavalid password"}) ;
-    }
+    catch(error){
 
-    
-})
+        return res.json({
+            message : error.message 
+        });
+    }
+});
